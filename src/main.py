d3cd45d5bc4c1a7ff3b024f8dba72305a41acc5d
JEREMY PUN <jeremy_ming@docker.com>
2026-01-03 21:42:02 +0800

    Added .venv folder

--- src/main.py
+++ src/main.py
@@ -1,15 +1,6 @@
-import requests
+from utils import *
 import time
-
-def get_crypto_price():
-    url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
-    try:
-        response = requests.get(url)
-        data = response.json()
-        price = data['bitcoin']['usd']
-        print(f"Current Bitcoin Price: ${price:,}")
-    except Exception as e:
-        print(f"Error fetching data: {e}")
+import os
 
 if __name__ == "__main__":
     print("Starting Bitcoin Tracker...")
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,22 @@
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+import requests
+import time
+
+def save_price_to_file(price):
+    # We save it in the 'src' folder which is mapped to your computer
+    file_path = "src/prices.log"
+    timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
+
+    with open(file_path, "a") as f:  # "a" means 'append' (add to the end)
+        f.write(f"TIMESTAMP: {timestamp} - PRICE: ${price}\n")
+    print(f"Saved to {file_path}")
+
+def get_crypto_price():
+    url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+    try:
+        response = requests.get(url)
+        data = response.json()
+        price = data['bitcoin']['usd']
+        print(f"Current Bitcoin Price: ${price:,}")
+        save_price_to_file(price)
+    except Exception as e:
+        print(f"Error: {e}")
